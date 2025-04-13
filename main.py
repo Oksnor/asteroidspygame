00c17476833ca0d6c543e40f7dd3629391dda5e0
Oksnor <oskarvanhan_@hotmail.com>
2025-04-13 16:08:28 +0200

    added bullets, timing on the bullets and being able to destory asteroids and bullets

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main(): 
     pygame.init()
@@ -17,15 +18,18 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     
     AsteroidField.containers = (updatable,)
     Asteroid.containers = (asteroids, updatable,drawable)
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
-    
+
 
     while True:
         for event in pygame.event.get():
@@ -36,6 +40,10 @@ def main():
             if asteroid.collision(player) == True:
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collision(shot) == True:
+                    asteroid.kill()
+                    shot.kill()
         screen.fill("black")
         for object in drawable:
             object.draw(screen)
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from circleshape import *
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
         # in the player class
     def triangle(self):
@@ -24,16 +26,29 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.move(dt)
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            if self.timer <= 0:
+                self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
+
+    
+        
+
